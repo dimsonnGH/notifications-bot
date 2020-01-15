ff3ead11758b0bc66b5bce7aee3695e812710c5b
dimsonnGH <dimsonn@mail.ru>
2020-01-15 19:32:22 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ import requests
 import logging
 import telegram
 
-print("hello world")
+def print_2_log(message_text, *args):
 
-#logging.basicConfig(format = '%(levelname)-8s [%(asctime)s]  %(message)s', filename = 'error_log.log', level=logging.DEBUG)
-logging.basicConfig(format = '%(levelname)-8s [%(asctime)s]  %(message)s', stream = sys.stdout, level=logging.DEBUG)
+  global error_counter
+  error_counter += 1
 
-logging.info('start bot')
-
+  logging.error(message_text.format(*args))
 
 TIMEOUT = 120
 MAX_ERROR_COUNT = 5
@@ -24,8 +23,65 @@ headers = {"Authorization":"Token " + DVMN_TOKEN}
 
 bot = telegram.Bot(token = TELEGRAM_TOKEN)
 
-message_text = 'start bot'
+#logging.basicConfig(format = '%(levelname)-8s [%(asctime)s]  %(message)s', filename = 'error_log.log')
+logging.basicConfig(format = '%(levelname)-8s [%(asctime)s]  %(message)s', stream = sys.stdout)
+
+timestamp = 0
+params = {}
+error_counter = 0
+
+while error_counter <= MAX_ERROR_COUNT:
+
+  if timestamp:
+    params = {'timestamp' : str(timestamp)}
+
+  try:
+
+    response = requests.get(url, headers = headers, timeout = TIMEOUT, params = params)
+    response.raise_for_status()
+
+  except requests.exceptions.ReadTimeout:
+
+    print_2_log('Request timeout. URL: {}', response.url)
+
+  except requests.exceptions.ConnectionError:
+
+    print_2_log('Connection error.')
+
+  except requests.exceptions.HTTPError:
+
+    print_2_log('HTTP error. Status code: {}. URL: {}', response.status_code, response.url)
+
+  else:
+
+    error_counter = 0
+
+    as_json = response.json()
+    if not 'status' in as_json:
+      continue
+
+    response_status = as_json['status']
+
+    if response_status == 'TIMEOUT':
+      
+      timestamp = as_json['timestamp_to_request']
+      continue
+
+    if response_status == 'found':
+
+      timestamp = as_json['last_attempt_timestamp']
+
+      for lesson in as_json['new_attempts']:
+
+        message_text = 'У вас проверили работу «{0}»'.format(lesson['lesson_title'])
           
-bot.send_message(chat_id = CHAT_ID, text = message_text)
+        bot.send_message(chat_id = CHAT_ID, text = message_text)
+        
+        if lesson['is_negative']:
+          message_text = 'К сожалению, в работе нашлись ошибки.'
+        else:
+          message_text = 'Преподавателю все понравилось, можно приступать к слеюдующему уроку!'
+
+        bot.send_message(chat_id = CHAT_ID, text = message_text)
 
 
